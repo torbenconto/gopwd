0821ecf3bb854bb292edc7ba32916f06e4438981
torbenconto <torbenmconto@gmail.com>
2024-07-03 20:46:54 -0400

    copy command ✅

---
+++ cmd/cp.go
@@ -0,0 +1,52 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/torbenconto/gopwd/internal/io"
+	"github.com/torbenconto/gopwd/internal/termio"
+	"path"
+)
+
+var cpCmd = &cobra.Command{
+	Use:   "cp [service] [new-service] [flags]",
+	Short: "Copy a password for a service",
+	Args:  cobra.ExactArgs(2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		service := args[0]
+		newService := args[1]
+
+		servicePath := path.Join(VaultPath, service) + ".gpg"
+		newServicePath := path.Join(VaultPath, newService) + ".gpg"
+
+		// Check if service exists
+		if !io.Exists(servicePath) {
+			return fmt.Errorf("service %s not found", service)
+		}
+
+		// Check if new service already exists
+		if io.Exists(newServicePath) {
+			fmt.Println("There is already a service at this name, would you like to overwrite it? This action cannot be undone.")
+			if confirm, _ := termio.ConfirmAction(); confirm {
+				err := io.CopyFile(servicePath, newServicePath)
+				if err != nil {
+					return fmt.Errorf("failed to copy file: %v", err)
+				}
+			} else {
+				fmt.Println("Aborted")
+			}
+		} else {
+			err := io.CopyFile(servicePath, newServicePath)
+			if err != nil {
+				return fmt.Errorf("failed to copy file: %v", err)
+			}
+			fmt.Printf("Copied %s to %s\n", service, newService)
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(cpCmd)
+}
--- internal/io/file.go
+++ internal/io/file.go
@@ -1,6 +1,9 @@
 package io
 
-import "os"
+import (
+	"io"
+	"os"
+)
 
 func CreateFile(file string) (*os.File, error) {
 	return os.Create(file)
@@ -16,6 +19,29 @@ func WriteToFile(file string, data []byte) error {
 
 func ReadFromFile(path string) ([]byte, error) {
 	return os.ReadFile(path)
+}
+
+func CopyFile(src, dst string) error {
+	// Open the source file for reading
+	sourceFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	// Create the destination file for writing
+	destFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	// Copy the content from the source file to the destination file
+	if _, err := io.Copy(destFile, sourceFile); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func CreateTempFile(content []byte) (*os.File, error) {
